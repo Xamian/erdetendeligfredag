063d74ebfb970d74d93c25218a6942901c6012d0
Max Kim Stagsted <max@max-it.dk>
2022-04-04 18:23:09 +0200

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	let isFriday = new Date().getDay() === 5;
+	let daysleft = 5 - new Date().getDay();
+</script>
+
+<h1>Er det endelig fredag?</h1>
+{#if isFriday}
+	<p>Ja, det er fredag!</p>
+{:else}
+	<p>Nej, det er ikke fredag, men der er kun {daysleft} dage til!</p>
+{/if}
